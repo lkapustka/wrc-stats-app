46563e68bdb210259a4b6d63c0e03b47f94ecf95
Łukasz Kapustka <lukaszkapustka@gmail.com>
2021-05-20 13:23:46 +0200

    feat: Add resetPasword function

--- src/components/PasswordReset.vue
+++ src/components/PasswordReset.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="modal">
     <div class="modal-content">
-      <div @click="$emit('close')" class="close">close</div>
+      <div @click="$emit('close')" class="close">Close</div>
       <h3>Reset Password</h3>
       <div v-if="!showSuccess">
         <p>Enter your email to reset your password</p>
         <form @submit.prevent>
           <input v-model.trim="email" type="email" placeholder="Email" />
         </form>
+        <p v-if="errorMsg !== ''" class="error">{{ errorMsg }}</p>
         <button @click="resetPassword" class="button">Reset</button>
       </div>
       <p v-else>Success! Check your email for a reset link.</p>
@@ -16,10 +17,32 @@
 </template>
 
 <script>
-import { defineComponent } from "@vue/composition-api";
-//import { auth } from "@/firebase";
+import { ref, defineComponent } from "@vue/composition-api";
+import { auth } from "@/firebase";
 
 export default defineComponent({
-  setup() {},
+  setup() {
+    const errorMsg = ref("");
+    const email = ref("");
+    const showSuccess = ref(false);
+
+    const resetPassword = async () => {
+      errorMsg.value = "";
+
+      try {
+        await auth.sendPasswordResetEmail(email.value);
+        showSuccess.value = true;
+      } catch (err) {
+        errorMsg.value = err.message;
+      }
+    };
+
+    return {
+      errorMsg,
+      email,
+      showSuccess,
+      resetPassword,
+    };
+  },
 });
 </script>
