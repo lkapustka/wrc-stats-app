a306550bba2a446587f573bbf3c8bdeaecfd6f53
Łukasz Kapustka <lukaszkapustka@gmail.com>
2021-05-20 13:25:05 +0200

    style: Add style for password reset

--- src/components/PasswordReset.vue
+++ src/components/PasswordReset.vue
@@ -46,3 +46,52 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.modal {
+  display: flex;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(#333, 0.5);
+
+  .modal-content {
+    position: relative;
+    margin: auto;
+    width: 100%;
+    max-width: 400px;
+    background: #fff;
+    padding: 40px;
+    border-radius: 5px;
+    box-shadow: 0 0 5px 0 rgba(#333, 0.5);
+    z-index: 999;
+
+    .close {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      padding: 5px;
+      cursor: pointer;
+
+      &:hover {
+        color: #000;
+      }
+    }
+
+    p {
+      margin: 15px 0;
+    }
+
+    .error {
+      font-size: 12px;
+      color: tomato;
+    }
+
+    .button {
+      width: 100%;
+    }
+  }
+}
+</style>
