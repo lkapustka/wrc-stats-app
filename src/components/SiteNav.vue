584f6a9ad672172832ed73c671291455db43791e
Łukasz Kapustka <lukaszkapustka@gmail.com>
2021-05-20 13:46:51 +0200

    feat: update the markup on the SiteNav component

---
+++ src/components/SiteNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <section>
+      <div class="col1">
+        <router-link to="/">
+          <h3>WRC Stats</h3>
+        </router-link>
+        <ul class="inline">
+          <li>
+            <router-link to="/">Dashboard</router-link>
+          </li>
+          <li>
+            <router-link to="/settings">Settings</router-link>
+          </li>
+          <li><a>Logout</a></li>
+        </ul>
+      </div>
+    </section>
+  </header>
+</template>
