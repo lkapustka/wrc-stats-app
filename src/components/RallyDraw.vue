f1dd662761401481c316b70fa02e9d0b1951358e
szasza31 <lukaszkapustka@gmail.com>
2021-06-27 17:34:15 +0200

    style: change font weight and elements position

--- src/components/RallyDraw.vue
+++ src/components/RallyDraw.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container">
-    <h2>WRC Rallies Draw</h2>
     <button class="button" @click="showRandomRally">Draw Rally</button>
     <div class="input">{{ randomRally }}</div>
   </div>
@@ -46,30 +45,28 @@ export default defineComponent({
 
 .container {
   display: grid;
-  grid-template-rows: 50px 50px 40px;
+  grid-template-rows: repeat(2, 50px);
   grid-template-columns: repeat(10, 1fr);
   column-gap: 10px;
   row-gap: 50px;
-}
 
-h2 {
-  grid-row: 2;
-  grid-column: 4 / 8;
-}
+  .button {
+    grid-row: 2;
+    grid-column: 4;
+  }
 
-.button {
-  grid-row: 3;
-  grid-column: 4;
-}
-
-.input {
-  background: lighten($color: $primary, $amount: 55);
-  border-radius: 3px;
-  text-align: center;
-  font-size: 1.2rem;
-  padding: 0.5rem 1rem;
-  min-width: 400px;
-  grid-row: 3;
-  grid-column: 5 / 8;
+  .input {
+    background: $secondary;
+    border-radius: 3px;
+    text-align: center;
+    font-size: 1rem;
+    padding: 0.5rem 1rem;
+    min-width: 400px;
+    grid-row: 2;
+    grid-column: 5 / 8;
+    color: $primary;
+    font-weight: bold;
+    line-height: 2.1rem;
+  }
 }
 </style>
