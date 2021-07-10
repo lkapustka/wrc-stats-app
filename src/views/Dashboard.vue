3fb8a2d060abb48903e58c2b61b94fe1fd38ebbb
szasza31 <lukaszkapustka@gmail.com>
2021-07-10 17:07:28 +0200

    feat: add sidebar and rallydraw components

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,19 +1,19 @@
 <template>
-  <div></div>
+  <div class="container">
+    <side-bar />
+    <rally-draw />
+  </div>
 </template>
 
 <script>
-import { defineComponent } from "@vue/composition-api";
+import SideBar from "../components/SideBar.vue";
+import RallyDraw from "../components/RallyDraw.vue";
 
-export default defineComponent({
+export default {
+  components: { RallyDraw, SideBar },
+
   setup() {},
-});
+};
 </script>
 
-<style lang="scss" scoped>
-div {
-  width: 100vw;
-  height: 100vh;
-  background-color: black;
-}
-</style>
+<style lang="scss" scoped></style>
