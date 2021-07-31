d6a2c504d73816a5a2156fa19e79e2af044bca3e
szasza31 <lukaszkapustka@gmail.com>
2021-07-31 23:58:45 +0200

    refactor: change layout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,69 @@
 <template>
-  <div class="login">
+  <div class="container">
+    <div class="bg">
+      <div class="bg__img"></div>
+    </div>
+
+    <transition name="fade" mode="out-in" appear>
+      <form class="form" v-if="showLoginForm" key="true" @submit.prevent>
+        <h1 class="form__title">Welcome Back</h1>
+        <div class="form__test">
+          <input
+            class="form__input"
+            v-model.trim="loginForm.email"
+            type="text"
+            placeholder="E-mail"
+          />
+        </div>
+        <input
+          class="form__input"
+          v-model.trim="loginForm.password"
+          type="password"
+          placeholder="Password"
+        />
+        <button @click="login" type="submit" class="btn form__btn">Log In</button>
+      </form>
+
+      <form class="form" v-else key="false" @submit.prevent>
+        <h1 class="form__title">Get Started</h1>
+        <input
+          class="form__input"
+          v-model.trim="signupForm.name"
+          type="text"
+          placeholder="Your nick"
+        />
+        <!-- <input
+        class="form__input"
+        v-model.trim="signupForm.friend"
+        type="text"
+        placeholder="Your friend name"
+      /> -->
+        <input
+          class="form__input"
+          v-model.trim="signupForm.email"
+          type="text"
+          placeholder="E-mail"
+        />
+        <input
+          class="form__input"
+          v-model.trim="signupForm.password"
+          type="password"
+          placeholder="Password"
+        />
+        <button class="btn form__btn" @click="signup">Sign Up</button>
+      </form>
+    </transition>
+
+    <div class="extras" v-if="showLoginForm" key="true">
+      <a class="extras__link" @click="togglePaswordReset">Forgot Password?</a>
+      <a class="extras__link" @click="toggleForm">Sign Up</a>
+    </div>
+    <div class="extras extras--signup" v-else key="false">
+      <a class="extras__link" @click="toggleForm">Back to Log In</a>
+    </div>
+  </div>
+
+  <!-- <div class="login">
     <PasswordReset
       v-if="showPasswordReset"
       @close="togglePaswordReset"
@@ -64,15 +128,15 @@
         </form>
       </div>
     </section>
-  </div>
+  </div> -->
 </template>
 
 <script>
 import { reactive, ref, inject } from "@vue/composition-api";
-import PasswordReset from "@/components/PasswordReset.vue";
+//import PasswordReset from "@/components/PasswordReset.vue";
 
 export default {
-  components: { PasswordReset },
+  //components: { PasswordReset },
 
   setup() {
     const store = inject("vuex-store");
@@ -130,55 +194,163 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/main.scss";
-
-.login {
-  background: linear-gradient(
-    to right,
-    $primary 0%,
-    $primary 50%,
-    white 50%,
-    white 100%
-  );
+.container {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
 }
 
-.col1,
-.col2 {
+.bg {
+  width: 100vw;
   height: 100vh;
-  padding-top: 30vh;
-}
+  background-color: rgba(0, 0, 0, 0.6);
 
-.col1 {
-  color: white;
-
-  p {
-    max-width: 510px;
-    line-height: 1.8;
-    font-weight: bold;
+  &__img {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    background-image: url("../../public/img/bg-login-small.jpg");
+    background-size: cover;
+    background-position: 20% 0;
+    z-index: -10;
   }
 }
 
-.col2 {
-  color: $primary;
+.form {
+  position: absolute;
+  display: flex;
+  width: 80%;
+  height: 80%;
+  flex-direction: column;
+  //justify-content: flex-start;
 
-  form {
-    margin: 0 auto;
-    max-width: 450px;
-    font-weight: bold;
+  &__title {
+    margin: 1em 0 1.5em;
+    font-size: 3.5rem;
+    align-self: center;
+    letter-spacing: 1px;
   }
 
-  .extras {
-    float: right;
-    text-align: right;
+  &__input {
+    display: block;
+    width: 100%;
+    margin-bottom: 1.5em;
+    padding: 0.5em 0;
+    border: none;
+    border-style: none none solid;
+    border-color: rgb(255, 255, 255);
+    color: white;
+    font-weight: bold;
+    font-size: 2rem;
+    font-family: inherit;
+    background-color: transparent;
+    letter-spacing: 1px;
 
-    a {
-      display: block;
-      margin-bottom: 0.5rem;
+    &::placeholder {
+      color: white;
+      font-weight: bold;
+      letter-spacing: 1px;
+      transition: opacity 2s;
+    }
+
+    &:focus {
+      outline: none;
+      //border-color: rgb(124, 123, 123);
+      color: white;
+      font-weight: bold;
     }
   }
+
+  &__btn {
+    margin: 2em 0;
+    background-color: rgba(23, 31, 192, 0.5);
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
 }
 
-.signup-form {
-  padding-top: 30vh;
+.extras {
+  position: absolute;
+  display: flex;
+  bottom: 0;
+  width: 100%;
+  justify-content: space-between;
+
+  &--signup {
+    justify-content: center;
+  }
+
+  &__link {
+    margin: 0.5em;
+    padding: 1em;
+    font-size: 1.6rem;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
 }
+
+.fade-enter-active {
+  transition: opacity 0.3s;
+}
+
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+// .login {
+//   background: linear-gradient(
+//     to right,
+//     $primary 0%,
+//     $primary 50%,
+//     white 50%,
+//     white 100%
+//   );
+// }
+
+// .col1,
+// .col2 {
+//   height: 100vh;
+//   padding-top: 30vh;
+// }
+
+// .col1 {
+//   color: white;
+
+//   p {
+//     max-width: 510px;
+//     line-height: 1.8;
+//     font-weight: bold;
+//   }
+// }
+
+// .col2 {
+//   color: $primary;
+
+//   form {
+//     margin: 0 auto;
+//     max-width: 450px;
+//     font-weight: bold;
+//   }
+
+//   .extras {
+//     float: right;
+//     text-align: right;
+
+//     a {
+//       display: block;
+//       margin-bottom: 0.5rem;
+//     }
+//   }
+// }
+
+// .signup-form {
+//   padding-top: 30vh;
+// }
 </style>
