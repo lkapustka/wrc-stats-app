535e5bbb4d0e8a7b6d6230032e34e4f37b898a0e
szasza31 <lukaszkapustka@gmail.com>
2021-08-15 23:32:44 +0200

    fix: change extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,11 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    "plugin:vue/vue3-recommended",
-    "**prettier**"
-  ],
+  extends: ["eslint:recommended", "plugin:vue/recommended", "prettier"],
   parserOptions: {
     parser: "babel-eslint",
+    ecmaVersion: "2021",
+    sourceType: "module",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
