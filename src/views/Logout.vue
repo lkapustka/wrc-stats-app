b2c3ce09bce8530bb7cf41076c0d41176cfcbc84
szasza31 <lukaszkapustka@gmail.com>
2021-08-26 22:03:13 +0200

    feat: add new components

---
+++ src/views/Logout.vue
@@ -0,0 +1,9 @@
+<script>
+import { defineComponent, inject } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    const store = inject("vuex-store");
+  },
+});
+</script>
---
+++ src/views/Rallies.vue
@@ -0,0 +1,11 @@
+<template>
+  <div />
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
