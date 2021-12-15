f3a970c102c201b9105ecd4b62088b3846604c30
Łukasz Kapustka <lukaszkapustka@gmail.com>
2021-12-15 17:40:47 +0100

    feat: add firebase

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+firebase.initializeApp(firebaseConfig);
+
+//utils
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+//collections
+const usersCollection = db.collection("users");
+
+export { db, auth, usersCollection };
