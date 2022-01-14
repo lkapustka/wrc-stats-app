2a54fb794bbf8f9a651fa13145bef52ae893c053
Łukasz Kapustka <lukaszkapustka@gmail.com>
2022-01-14 14:53:06 +0100

    refactor: add createRally method

--- src/components/RallyDraw.vue
+++ src/components/RallyDraw.vue
@@ -11,7 +11,7 @@
       >
         Draw Rally
       </button>
-      <buttonsCTA
+      <rally-draw-buttons
         v-else
         @accept="acceptRally"
         @discard="discardRally"
@@ -25,14 +25,14 @@
 </template>
 
 <script>
-import { inject, ref } from "@vue/composition-api";
+import { inject, ref, set } from "@vue/composition-api";
 import { useDebounceFn } from "@vueuse/core";
-import ButtonsCTA from "./UI Components/ButtonsCTA.vue";
+import RallyDrawButtons from "./UI Components/RallyDrawButtons.vue";
 import RallyDrawHistory from './RallyDrawHistory.vue';
 
 export default {
   name: "RallyDraw",
-  components: { ButtonsCTA, RallyDrawHistory },
+  components: { RallyDrawButtons, RallyDrawHistory },
 
   setup() {
     const isActive = ref(true);
@@ -45,40 +45,48 @@ export default {
     };
 
     const timeOfDay = ["Dawn", "Midday", "Evening", "Night"];
-    const drawRandomTimeofDay = () => {
-      const index = getRandom(1, timeOfDay.length);
-      return timeOfDay[index - 1];
+    const drawRandomTimeOfDay = () => {
+      const index = getRandom(0, timeOfDay.length - 1);
+      return timeOfDay[index];
     };
 
     const store = inject("vuex-store");
     const drawRandomRally = () => {
-      const id = getRandom(1, store.state.rallies.length);
+      const id = getRandom(1, store.getters.ralliesCount);
       return store.getters.getRally(id);
     };
 
+    const createRally = () => {
+      const rally = drawRandomRally();
+      set(rally, "time",  drawRandomTimeOfDay());
+      set(rally, "date", new Date().toLocaleString("en-GB"));
+      set(rally, "status", "Active");
+      return rally;
+    }
+
     const randomRally = ref("Click the button above to get started");
     const showRandomRally = useDebounceFn(() => {
-
       toggleButtons();
-
-      const rally = drawRandomRally();
-      rally.date = new Date().toLocaleString("en-GB");
-      const timeofDay = drawRandomTimeofDay();
-      randomRally.value = `${rally.country} - ${rally.name}  (${timeofDay})`;
+      const rally = createRally();
+      randomRally.value = `${rally.country} - ${rally.name}  (${rally.time})`;
 
       store.commit("setActiveRally", rally);
       store.commit("setRalliesHistory", rally);
-
     }, 200);
 
     const discardRally = () => {
       toggleButtons();
+      const rally = store.getters.getActiveRally;
+      rally.status = "Discarded";
       randomRally.value = "Click the button above to get started";
+
       store.commit("setActiveRally", null);
-    }
+    };
 
     const acceptRally = () => {
-      console.log("accept");
+      toggleButtons();
+      const rally = store.getters.getActiveRally;
+      rally.status = "Accepted";
     }
 
     return {
